59559284b2944e74fddecb66a3c63762ec65898a
Marta Locmele <martamarijalocmele@gmail.com>
2024-05-13 19:46:21 +0300

    partly refactored menu

--- src/app/components/Menu/Menu.jsx
+++ src/app/components/Menu/Menu.jsx
@@ -3,30 +3,41 @@ import React from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import styles from './Menu.module.css'
+import { useRouter } from 'next/router'
+
 
 export default function Menu() {
 
-  const pathname = usePathname()
+  // const router = useRouter()
+  // const pathname = usePathname()
+
+  const menuItems = [
+    { href: '/', label: 'FASHION COLLECTIONS' },
+    { href: '/injured', label: 'INJURED' },
+    { href: '/mothers', label: 'MOTHERS' },
+    { href: '/somnambulance', label: 'SOMNAMBULANCE OF THE CITY' },
+    { href: '/nightmares', label: 'NIGHTMARES OF LONELY LIGHTOUSE KEEPER' },
+    { href: '/', label: 'COLLABORATIONS' },
+    { href: '/XXX24', label: 'XXX24' },
+    { href: '/aj', label: 'AJ POWER PROJECT RECYCLING' },
+    { href: '/xxx22', label: 'XXX22' },
+    { href: '/volga', label: 'VOLGA VINTAGE' },
+    { href: '/', label: 'PUBLICITY' },
+    { href: '/lofficiel', label: `L'OFFICIEL BALTIC` },
+    { href: '/santa', label: 'SANTA MAGAZINE' },
+    { href: '/lilit', label: 'LILIT LOOK' },
+    { href: '/', label: 'CREATIVE DIRECTION' },
+  ]
 
   return (
     <div className={styles.container}>
       <div className={styles.fixedContainer}>
-        <h1 className={styles.header}>Loreta<br />Asermane</h1>
-        <Link href="/first" className={styles.categories}>FASHION COLLECTIONS</Link>
-        <Link href="/injured" className={`${styles.sub} ${styles.categories}`}>INJURED</Link>
-        <Link href="/mothers" className={`${styles.sub} ${styles.categories}`}>MOTHERS</Link>
-        <Link href="/somnambulance" className={`${styles.sub} ${styles.categories}`}>SOMNAMBULANCE OF THE CITY</Link>
-        <Link href="/nightmares" className={`${styles.sub} ${styles.categories}`}>NIGHTMARES OF LONELY LIGHTOUSE KEEPER</Link>
-        <Link href="/first" className={styles.categories}>COLLABORATIONS</Link>
-        <Link href="/first" className={`${styles.sub} ${styles.categories}`}>XXX24</Link>
-        <Link href="/first" className={`${styles.sub} ${styles.categories}`}>AJ POWER PROJECT RECYCLING</Link>
-        <Link href="/first" className={`${styles.sub} ${styles.categories}`}>XXX22</Link>
-        <Link href="/first" className={`${styles.sub} ${styles.categories}`}>VOLGA VINTAGE</Link>
-        <Link href="/first" className={styles.categories}>PUBLICITY</Link>
-        <Link href="/first" className={`${styles.sub} ${styles.categories}`}>L'OFFICIEL BALTIC</Link>
-        <Link href="/first" className={`${styles.sub} ${styles.categories}`}>SANTA MAGAZINE</Link>
-        <Link href="/first" className={`${styles.sub} ${styles.categories}`}>LILIT LOOK</Link>
-        <Link href="/first" className={styles.categories}>CREATIVE DIRECTION</Link>
+        <Link href={'/'} className={styles.header}>Loreta <br /> Asermane</Link>
+        {menuItems.map((menuItem, index) => (
+          <Link href={menuItem.href} key={index} className={styles.categories}>
+            <p className={styles.categories}>{menuItem.label}</p>
+          </Link>
+        ))}
       </div>
     </div >
   )
