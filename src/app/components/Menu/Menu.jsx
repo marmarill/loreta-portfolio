8a2c8cf80e360fbbcda474a625dacba909c597be
Marta Locmele <martamarijalocmele@gmail.com>
2024-05-13 21:14:50 +0300

    added current section highlight

--- src/app/components/Menu/Menu.jsx
+++ src/app/components/Menu/Menu.jsx
@@ -8,35 +8,55 @@ import { useRouter } from 'next/router'
 
 export default function Menu() {
 
-  // const router = useRouter()
-  // const pathname = usePathname()
+
+  const currentPath = usePathname()
 
   const menuItems = [
-    { href: '/', label: 'FASHION COLLECTIONS' },
-    { href: '/injured', label: 'INJURED' },
-    { href: '/mothers', label: 'MOTHERS' },
-    { href: '/somnambulance', label: 'SOMNAMBULANCE OF THE CITY' },
-    { href: '/nightmares', label: 'NIGHTMARES OF LONELY LIGHTOUSE KEEPER' },
-    { href: '/', label: 'COLLABORATIONS' },
-    { href: '/XXX24', label: 'XXX24' },
-    { href: '/aj', label: 'AJ POWER PROJECT RECYCLING' },
-    { href: '/xxx22', label: 'XXX22' },
-    { href: '/volga', label: 'VOLGA VINTAGE' },
-    { href: '/', label: 'PUBLICITY' },
-    { href: '/lofficiel', label: `L'OFFICIEL BALTIC` },
-    { href: '/santa', label: 'SANTA MAGAZINE' },
-    { href: '/lilit', label: 'LILIT LOOK' },
-    { href: '/', label: 'CREATIVE DIRECTION' },
+    {
+      label: 'FASHION COLLECTIONS', items: [
+        { path: '/injured', label: 'INJURED' },
+        { path: '/mothers', label: 'MOTHERS' },
+        { path: '/somnambulance', label: 'SOMNAMBULANCE OF THE CITY' },
+        { path: '/nightmares', label: 'NIGHTMARES OF LONELY LIGHTOUSE KEEPER' }
+      ]
+    },
+    {
+      label: 'COLLABORATIONS', items: [
+        { path: '/XXX24', label: 'XXX24' },
+        { path: '/aj', label: 'AJ POWER PROJECT RECYCLING' },
+        { path: '/xxx22', label: 'XXX22' },
+        { path: '/volga', label: 'VOLGA VINTAGE' }
+      ]
+    },
+    {
+      label: 'PUBLICITY', items: [
+        { path: '/lofficiel', label: `L'OFFICIEL BALTIC` },
+        { path: '/santa', label: 'SANTA MAGAZINE' },
+        { path: '/lilit', label: 'LILIT LOOK' }
+      ]
+    },
+    { label: 'CREATIVE DIRECTION', items: [] }
   ]
+
+  const isActive = (path) => {
+    return currentPath === path
+  }
 
   return (
     <div className={styles.container}>
       <div className={styles.fixedContainer}>
         <Link href={'/'} className={styles.header}>Loreta <br /> Asermane</Link>
-        {menuItems.map((menuItem, index) => (
-          <Link href={menuItem.href} key={index} className={styles.categories}>
-            <p className={styles.categories}>{menuItem.label}</p>
-          </Link>
+        {menuItems.map((category, index) => (
+          <div key={index}>
+            <p className={styles.category}>{category.label}</p>
+            <div className={styles.sub}>
+              {category.items.map((subCategory, subIndex) => (
+                <Link href={subCategory.path} key={subIndex} >
+                  <p className={isActive(subCategory.path) ? styles.active : styles.sub}>{subCategory.label}</p>
+                </Link>
+              ))}
+            </div>
+          </div>
         ))}
       </div>
     </div >
