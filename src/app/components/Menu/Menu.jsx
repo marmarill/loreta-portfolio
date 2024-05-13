588e2ddad40327cc752f80ea26a5ef0feab1b394
Marta Locmele <martamarijalocmele@gmail.com>
2024-05-13 17:29:17 +0300

    added fashion collection sub category routes

--- src/app/components/Menu/Menu.jsx
+++ src/app/components/Menu/Menu.jsx
@@ -14,9 +14,9 @@ export default function Menu() {
         <h1 className={styles.header}>Loreta<br />Asermane</h1>
         <Link href="/first" className={styles.categories}>FASHION COLLECTIONS</Link>
         <Link href="/injured" className={`${styles.sub} ${styles.categories}`}>INJURED</Link>
-        <Link href="/first" className={`${styles.sub} ${styles.categories}`}>MOTHERS</Link>
-        <Link href="/first" className={`${styles.sub} ${styles.categories}`}>SOMNAMBULANCE OF THE CITY</Link>
-        <Link href="/first" className={`${styles.sub} ${styles.categories}`}>NIGHTMARES OF LONELY LIGHTOUSE KEEPER</Link>
+        <Link href="/mothers" className={`${styles.sub} ${styles.categories}`}>MOTHERS</Link>
+        <Link href="/somnambulance" className={`${styles.sub} ${styles.categories}`}>SOMNAMBULANCE OF THE CITY</Link>
+        <Link href="/nightmares" className={`${styles.sub} ${styles.categories}`}>NIGHTMARES OF LONELY LIGHTOUSE KEEPER</Link>
         <Link href="/first" className={styles.categories}>COLLABORATIONS</Link>
         <Link href="/first" className={`${styles.sub} ${styles.categories}`}>XXX24</Link>
         <Link href="/first" className={`${styles.sub} ${styles.categories}`}>AJ POWER PROJECT RECYCLING</Link>
---
+++ src/app/mothers/page.js
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+export default function Home() {
+  return <>
+    <Layout>
+      <h1>MOTHERS</h1>
+    </Layout>
+  </>
+}
