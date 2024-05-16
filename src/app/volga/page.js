8db77520eeb51d9c4b7a0907d96bfa0baca13957
Marta Locmele <martamarijalocmele@gmail.com>
2024-05-16 15:45:11 +0300

    added projects

---
+++ src/app/volga/page.js
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+export default function Home() {
+  return <>
+    <Layout>
+      <h1>SOMNAMBULANCE</h1>
+    </Layout>
+  </>
+}
